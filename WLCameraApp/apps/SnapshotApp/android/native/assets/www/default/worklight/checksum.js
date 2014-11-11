5261eb34db61adeee4b92beb45fbd7de7769769e
Abdul Ahad <withabdulahad@gmail.com>
2014-11-11 23:48:37 +0500

    Worklight Camera App
    This small app demonstrate the Cordova Camera feature

---
+++ WLCameraApp/apps/SnapshotApp/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"date":1415730448947,"machine":"OmerSystem","checksum":791694581};
+/* Date: Tue Nov 11 23:27:28 PKT 2014 */
---
+++ WLCameraApp/apps/SnapshotApp/common/js/main.js
@@ -0,0 +1,50 @@
+function wlCommonInit(){
+	/*
+	 * Use of WL.Client.connect() API before any connectivity to a Worklight Server is required. 
+	 * This API should be called only once, before any other WL.Client methods that communicate with the Worklight Server.
+	 * Don't forget to specify and implement onSuccess and onFailure callback functions for WL.Client.connect(), e.g:
+	 *    
+	 *    WL.Client.connect({
+	 *    		onSuccess: onConnectSuccess,
+	 *    		onFailure: onConnectFailure
+	 *    });
+	 *     
+	 */
+	
+	// Common initialization code goes here
+	
+}
+
+function onSuccess(imageData) {
+	alert( imageData );
+	var smallImage = document.getElementById('ImgPlaceholder'); 
+	smallImage.style.display = 'block';
+	smallImage.src = imageData;
+	 
+}
+
+function onFail(message) {
+	alert('Failed because: ' + message);
+}	
+
+//Use from Camera
+$("#takePictureBtn").on("touchend", function() {
+	alert("taking pic");
+	navigator.camera.getPicture(onSuccess, onFail, { 
+		quality: 50,
+		sourceType: Camera.PictureSourceType.CAMERA,
+		destinationType: Camera.DestinationType.FILE_URI
+	});
+
+});
+
+//Use from Library
+$("#openGalleryBtn").on("touchend", function() {
+	alert("browser gallery");
+	navigator.camera.getPicture(onSuccess, onFail, { 
+		quality: 50,
+		sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+		destinationType: Camera.DestinationType.FILE_URI
+	});
+
+});
